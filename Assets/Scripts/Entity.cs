50feb1c7a219b2e9562c7f88403a2401b423c88a
FOURNIER <nathan.fournier31@gmail.com>
2022-05-28 03:58:49 +0200

    WIP Integration IA

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ashlight.james_strike_again
+{
+    public abstract class Entity : MonoBehaviour
+    {
+        public event Action OnHealthChanged;
+        public event Action OnDeath;
+        [SerializeField] private float health;
+
+        public float Health { 
+            get { return health; } 
+            protected set {
+                health = value;
+                OnHealthChanged.Invoke();
+                if (health <= 0)
+                {
+                    OnDeath.Invoke();
+                }
+            } }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public abstract void TakeDamage(float damage);
+    }
+}
---
+++ Assets/Scripts/IAScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ashlight.james_strike_again
+{
+    public class IAScript : Entity
+    {
+        [SerializeField] private float fireRate;
+        [SerializeField] private float range;
+        [SerializeField] private float damage;
+        [SerializeField] private LayerMask playerMask;
+        //[SerializeField] private float health;
+
+        [SerializeField] private Collider collider;
+        [SerializeField] private Transform bulletOrigin;
+        [SerializeField] private GameObject bullet;
+
+        private GameObject player;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (IsPlayerInRange())
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            Debug.Log("AQueCoucou");
+        }
+
+
+        private bool IsPlayerInRange ()
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) <= range)
+            {
+                Debug.Log("EstAPortée");
+                RaycastHit hit;
+                Debug.DrawRay(transform.position, transform.forward, Color.red);
+                //Le joueur est à portée de l'IA on vérifie si il y a un obstacle entre les deux
+                if (Physics.Raycast(transform.position, transform.forward, out hit, float.MaxValue, playerMask))
+                {
+                    Debug.Log(hit);
+                    return hit.collider.gameObject.CompareTag("Player");
+                }
+            }
+            return false; 
+        }
+
+        public override void TakeDamage(float damage)
+        {
+            Health -= damage;
+        }
+    }
+}
